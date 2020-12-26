6c7cf6a9c74d1e7cebba54230ad9e8c932a6db33
sushmithashiv24 <60508097+sushmithashiv24@users.noreply.github.com>
2020-12-26 17:30:17 +0530

    update

---
+++ app.js
@@ -0,0 +1,75 @@
+const bill = document.querySelector('#bill-amt');
+const cash = document.querySelector('#cash');
+const btnNext = document.querySelector('#btn-next');
+const btnCheck = document.querySelector('#btn-check');
+const noOfNotes = document.querySelectorAll('.noOfNotes');
+const checkDiv = document.querySelector('#cash-div');
+const returnChange = document.querySelector('.changeReturn');
+const loader = document.querySelector('#loading');
+
+let notes = [2000, 500, 100, 20, 10, 5, 1];
+let returnNotes = [];
+//  billAmt;
+// let cashAmt;
+btnNext.addEventListener('click', function() {
+    // loader.className = "show";
+    // billAmt = bill.value;
+    // if (billAmt === '') {
+    //     alert('Please, Enter Billing Amount!');
+    // } else {
+    checkDiv.className = "show";
+    // }
+})
+
+btnCheck.addEventListener('click', function() {
+    let billAmt = bill.value;
+    let cashAmt = cash.value;
+    for (const notes of noOfNotes) {
+        notes.innerText = '';
+        // billAmt.value = '';
+        // cashAmt.value = '';
+    }
+
+    console.log(billAmt);
+    console.log(cashAmt);
+    if (billAmt === '') {
+        alert('Please, Enter Billing Amount!');
+    } else if (cashAmt === '') {
+        alert('Please, Enter Cash Amount!');
+    } else if (billAmt > cashAmt) {
+        // console.log(billAmt);
+        // console.log(cashAmt);
+        alert('Unsufficient cash, please enter adequate cash amount!');
+    } else if (cashAmt === billAmt) {
+        alert('No Change!');
+    } else {
+        loader.className = "show";
+        // returnChange.className = "hide";
+        setTimeout(function() {
+            loader.className = loader.className.replace("show", "");
+            returnChange.className = output.className.replace("", "show");
+        }, 1000);
+        let returnAmount = parseInt(cashAmt) - parseInt(billAmt);
+        console.log(returnAmount);
+
+        for (let i = 0; i < notes.length; i++) {
+            returnNotes[i] = Math.floor(returnAmount / notes[i]);
+            returnAmount = returnAmount - notes[i] * returnNotes[i];
+            noOfNotes[i].innerText = returnNotes[i];
+            if (returnAmount === 0) {
+                break;
+            }
+        }
+
+        for (const notes of noOfNotes) {
+            if (notes.innerText == '0') {
+                notes.innerText = '';
+            }
+        }
+
+        console.log(returnNotes);
+        console.log(noOfNotes);
+
+        returnNotes = [];
+    }
+});
